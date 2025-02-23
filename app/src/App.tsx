b859af8137525d8e36c28b8d1b178e74cd44283d
SarahOtmane <sarahotmane02@gmail.com>
2025-02-23 23:36:01 +0100

    Ajout de la configuration de routage avec React Router pour la page d'accueil

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,17 +1,16 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import './App.css';
+import { Home } from '@mui/icons-material';
 
 const App: React.FC = () => {
     return (
-        // <Router>
-        //     <Routes>
-        //         <Route path='/' element={<LandingPage />} />
-        //         <Route path='/home' element={<Home />} />
-        //         <Route path='/profil' element={<Profil />} />
-        //     </Routes>
-        // </Router>
-        <></>
+        <Router>
+            <Routes>
+                <Route path='/' element={<Home />} />
+            </Routes>
+        </Router>
     );
 }
 
