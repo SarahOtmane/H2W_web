208ec9b11668116bb3cfeb6881d0eec0a51f7e34
SarahOtmane <sarahotmane02@gmail.com>
2025-02-24 00:29:14 +0100

    import du composant Button dans la home page pour suivre les modifs du composant

--- app/src/pages/Home.tsx
+++ app/src/pages/Home.tsx
@@ -1,9 +1,11 @@
+import Button from "../components/Button";
 
 
 const Home: React.FC = () => {
     return (
         <div>
             <h1>Home</h1>
+            <Button />
         </div>
     );
 }
