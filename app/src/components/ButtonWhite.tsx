04408d4c01ddccca5191e7cc6a7b5d93244a091e
SarahOtmane <sarahotmane02@gmail.com>
2025-02-24 18:28:52 +0100

    Mise à jour du composant ButtonWhite pour modifier les styles de bouton, ajout d'une bordure grise et ajustement des classes CSS

--- app/src/components/ButtonWhite.tsx
+++ app/src/components/ButtonWhite.tsx
@@ -7,7 +7,7 @@ interface ButtonProps {
 
 const ButtonWhite: React.FC<ButtonProps> = ({text, icon}) => {
     return (
-        <button className={`flex items-center justify-center px-5 py-3  text-black bg-white rounded-[2rem] hover:text-white hover:bg-black border-orange hover:border-black border-1`}>
+        <button className={`flex items-center justify-center px-5 py-3 bg-white border-gray-border border-1  text-black rounded-[2rem] hover:text-white hover:bg-black hover:border-black `}>
             {icon && <img src={icon} alt='icon' className="w-5 h-5 mr-1" />}
             {icon && <span>&nbsp;</span>}
             {text}
