df1d473612455604d6ddbd33af34623b0599b9ba
SarahOtmane <sarahotmane02@gmail.com>
2025-02-24 18:32:13 +0100

    Ajout de la gestion des clics dans les composants ButtonBlack et ButtonWhite

--- app/src/components/ButtonBlack.tsx
+++ app/src/components/ButtonBlack.tsx
@@ -3,11 +3,12 @@ import '../App.css';
 interface ButtonProps {
     text: string;
     icon?: string;
+    handleClick?: () => void;
 }
 
-const ButtonBlack: React.FC<ButtonProps> = ({text, icon}) => {
+const ButtonBlack: React.FC<ButtonProps> = ({text, icon, handleClick}) => {
     return (
-        <button className={`flex items-center justify-center px-5 py-3  text-white bg-black rounded-[2rem] hover:text-black hover:bg-white border-black border-1`}>
+        <button onClick={handleClick} className={`flex items-center justify-center px-5 py-3  text-white bg-black rounded-[2rem] hover:text-black hover:bg-white border-black border-1`}>
             {icon && <img src={icon} alt='icon' className="w-5 h-5 mr-1" />}
             {icon && <span>&nbsp;</span>}
             {text}
