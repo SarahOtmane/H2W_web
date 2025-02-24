40ae09a7c4d23df2dc74cd6718150c1c438de2b9
SarahOtmane <sarahotmane02@gmail.com>
2025-02-24 12:32:38 +0100

    Ajout du composant ButtonWhite sur la page d'accueil et importation de la feuille de style App.css

--- app/src/pages/Home.tsx
+++ app/src/pages/Home.tsx
@@ -1,12 +1,19 @@
 import ButtonBlack from "../components/ButtonBlack";
 import icon from "../assets/react.svg"
 
+import '../App.css';
+import ButtonWhite from "../components/ButtonWhite";
+
 
 const Home: React.FC = () => {
     return (
         <div>
             <h1>Home</h1>
             <ButtonBlack
+                text='Click me'
+                icon={icon}
+            />
+            <ButtonWhite
                 text='Click me'
                 icon={icon}
             />
