b7396ab90fc2497432365ec82ba9b4d586dd4ab0
SarahOtmane <sarahotmane02@gmail.com>
2025-02-24 12:31:58 +0100

    Correction de la configuration des plugins dans vite.config.ts

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),],
   server: {
     host: "0.0.0.0", // Permet d'accéder au serveur depuis l'extérieur du conteneur
     port: 5173, // Assure que Vite écoute sur ce port
