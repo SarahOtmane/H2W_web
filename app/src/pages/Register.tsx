1e2d8951e86edb6ee2aed9c57038f40e20d80090
SarahOtmane <sarahotmane02@gmail.com>
2025-02-25 22:57:26 +0100

    Ajout du composant RegisterStudent et mise à jour de l'interface d'inscription

--- app/src/pages/Register.tsx
+++ app/src/pages/Register.tsx
@@ -1,38 +1,68 @@
+import { useState } from "react";
+
 import PageTitle from "../components/titles/PageTitle";
 import Pg from "../components/Pg";
 import RegisterCard from "../components/cards/RegisterCard";
 import Icon from "../Icon";
+import RegisterStudent from "../components/forms/RegisterStudent";
 
 
 const Register: React.FC = () => {
+    const [selected, setSelected] = useState('etudiant');
+    const [information, setInformation] = useState({
+        prenom: '',
+        nom: '',
+        email: '',
+        numero: '',
+        motDePasse: '',
+        confirmationMotDePasse: '',
+        nomEntreprise: '',
+        secteurActivite: '',
+        adresse: '',
+        complementAdresse: '',
+        ville: '',
+        codePostal: '',
+        siret: '',
+        nomEcole: '',
+    })
+
     return(
-        <main className="flex flex-row justify-center h-screen bg-gray-background w-full px-28 pt-50">
-            <div className="bg-lilas h-full w-1/3 rounded-">
+        <main className="flex flex-row justify-center bg-gray-background w-full px-40 pt-50 mb-20">
+            <div className="bg-lilas w-2/5 rounded-l-[2rem]">
             </div>
-            <section className="flex flex-col justify-center items-center w-2/3 bg-white">
+            <div className="flex flex-col h-max items-center w-3/5 bg-white px-10 py-12 rounded-[2rem] ml-[-2rem]">
                 <PageTitle text='Créer un compte'/>
                 <Pg text='Trouvez le professionnel parfait pour répondre à vos besoins quotidiens.'/>
-                <div className="flex flex-row justify-center items-center">
+                <div className="flex flex-row justify-center items-center mt-8">
                     <RegisterCard 
                         icon={<Icon name="registerStudent" />}
                         titre='Étudiant' 
                         description="Crée ton compte étudiant et accède à un portfolio interactif pour mettre en valeur tes compétences." 
-                        selected={true}
+                        style="mr-4"
+                        selected={selected}
+                        setSelected= {setSelected}
+                        select='etudiant'
                     />
                     <RegisterCard
                         icon={<Icon name="registerCompany" />}
                         titre='Entreprise'
                         description="Créez un compte entreprise et publiez vos offres pour attirer les talents adaptés à vos besoins."
-                        selected={false}
+                        style="mr-4"
+                        selected={selected}
+                        setSelected= {setSelected}
+                        select='entreprise'
                     />
                     <RegisterCard
                         icon={<Icon name="registerSchool" />}
-                        titre='Freelance'
-                        description="Créez un compte freelance et"
-                        selected={false}
+                        titre='École'
+                        description="Créez un compte école et accompagnez vos étudiants dans la recherche d’alternance."
+                        selected={selected}
+                        setSelected= {setSelected}
+                        select='ecole'
                     />
                 </div>
-            </section>
+                <RegisterStudent information={information} setInformation={setInformation} select="student" />
+            </div>
         </main>
     )
 }
