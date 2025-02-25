dc039e1aee008868ad6ef3ead606d1f1c60b6d04
SarahOtmane <sarahotmane02@gmail.com>
2025-02-25 20:05:22 +0100

    Modification du composant RegisterCard pour accepter un élément React comme icône et ajout d'une propriété de style optionnelle

--- app/src/components/cards/RegisterCard.tsx
+++ app/src/components/cards/RegisterCard.tsx
@@ -1,23 +1,28 @@
+import React from 'react';
 import Pg from "../Pg";
 
 interface RegisterCardProps {
-    icon: string;
+    icon: React.ReactElement;
     titre: string;
     description: string;
     selected: boolean;
+    style?: string;
 }
 
-
-const RegisterCard: React.FC<RegisterCardProps> = ({icon, titre, description, selected}) =>{
-    return(
-        <div className={`flex flex-col w-1/3 items-center p-2 rounded-2  border-1 ${selected? 'bg-lilas bg-opacity-20 border-lilas' : 'bg-white border-black'}`}>
+const RegisterCard: React.FC<RegisterCardProps> = ({icon, titre, description, selected, style}) => {
+    return (
+        <div className={`
+            flex flex-col w-1/ py-4 px-6 rounded-[1rem] border-1 border-solid h-full
+            ${selected ? 'border-lilas border-2 bg-lilas-light' : 'bg-white border-black'}
+            ${style? style : ''}
+        `}>
             <article className="flex flex-row items-center">
-                <img src={icon} alt={titre}/>
-                <p className="font-Jakarta-bold text-sm sm:text-base md:text-lg lg:text-body">{titre}</p>
+                {icon}
+                <p className="font-Jakarta-bold text-sm sm:text-base md:text-lg lg:text-body ml-3">{titre}</p>
             </article>
-            <Pg text={description}/>
+            <Pg text={description} style="mt-3"/>
         </div>
-    )
+    );
 }
 
 export default RegisterCard;
