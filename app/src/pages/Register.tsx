783cdf5b6c2073503a6dd336ae856d6e7e44289f
SarahOtmane <sarahotmane02@gmail.com>
2025-02-26 00:11:07 +0100

    Ajout de titres de section et d'un sous-titre dans le composant d'inscription

--- app/src/pages/Register.tsx
+++ app/src/pages/Register.tsx
@@ -5,6 +5,8 @@ import Pg from "../components/Pg";
 import RegisterCard from "../components/cards/RegisterCard";
 import Icon from "../Icon";
 import RegisterStudent from "../components/forms/RegisterStudent";
+import SectionTitle from "../components/titles/SectionTitle";
+import Subtitle from "../components/titles/Subtitle";
 
 
 const Register: React.FC = () => {
@@ -27,13 +29,29 @@ const Register: React.FC = () => {
     })
 
     return(
-        <main className="flex flex-row justify-center bg-gray-background w-full px-40 pt-50 mb-20">
-            <div className="bg-lilas w-2/5 rounded-l-[2rem]">
+        <main className="flex flex-row justify-center bg-gray-background w-full px-34 pt-50 mb-20">
+            <div className=" w-2/5 rounded-l-[2rem] bg-[url(./assets/images/register_back.png)] bg-cover bg-no-repeat relative flex flex-col justify-center items-center pb-40">
+                <div className="w-9/12 m-auto mt-14">
+                    <SectionTitle text='Étudiants, entreprises, écoles :' style="text-white text-center font-Jakarta-extra-bold" /> 
+                    <SectionTitle text='connectons les talents de demain !' style="text-center font-Jakarta-extra-bold" />
+                </div>
+                <div className="relative flex flex-col items-end mt-10 mr-10">
+                    <svg className="absolute -top-5 right-10" width="36" height="31" viewBox="0 0 36 31" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M19.7113 1.82936C18.9331 0.542796 17.0668 0.542798 16.2887 1.82936L0.783188 27.4649C-0.0230166 28.7978 0.936741 30.5 2.49451 30.5H33.5055C35.0633 30.5 36.023 28.7978 35.2168 27.4649L19.7113 1.82936Z" fill="white"/>
+                        <path d="M19.7113 1.82936C18.9331 0.542796 17.0668 0.542798 16.2887 1.82936L0.783188 27.4649C-0.0230166 28.7978 0.936741 30.5 2.49451 30.5H33.5055C35.0633 30.5 36.023 28.7978 35.2168 27.4649L19.7113 1.82936Z" fill="white"/>
+                        <path d="M19.7113 1.82936C18.9331 0.542796 17.0668 0.542798 16.2887 1.82936L0.783188 27.4649C-0.0230166 28.7978 0.936741 30.5 2.49451 30.5H33.5055C35.0633 30.5 36.023 28.7978 35.2168 27.4649L19.7113 1.82936Z" fill="white"/>
+                        <path d="M19.7113 1.82936C18.9331 0.542796 17.0668 0.542798 16.2887 1.82936L0.783188 27.4649C-0.0230166 28.7978 0.936741 30.5 2.49451 30.5H33.5055C35.0633 30.5 36.023 28.7978 35.2168 27.4649L19.7113 1.82936Z" fill="white"/>
+                        <path d="M19.7113 1.82936C18.9331 0.542796 17.0668 0.542798 16.2887 1.82936L0.783188 27.4649C-0.0230166 28.7978 0.936741 30.5 2.49451 30.5H33.5055C35.0633 30.5 36.023 28.7978 35.2168 27.4649L19.7113 1.82936Z" fill="white"/>
+                    </svg>
+                    <div className="bg-white w-max px-10 py-5 rounded-[2rem] m-auto">
+                        <Subtitle text="Bienvenue sur Hive2Work !" />
+                    </div>
+                </div>
             </div>
-            <div className="flex flex-col h-max items-center w-3/5 bg-white px-10 py-12 rounded-[2rem] ml-[-2rem]">
+            <div className="flex flex-col h-max items-center w-3/5 bg-white px-10 py-12 rounded-[2rem] ml-[-2rem] z-30">
                 <PageTitle text='Créer un compte'/>
                 <Pg text='Trouvez le professionnel parfait pour répondre à vos besoins quotidiens.'/>
-                <div className="flex flex-row justify-center items-center mt-8">
+                <div className="flex flex-row justify-center items-stretch mt-8 w-full">
                     <RegisterCard 
                         icon={<Icon name="registerStudent" />}
                         titre='Étudiant' 
