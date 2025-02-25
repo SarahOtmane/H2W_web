c8782384a7fa81bd4cfbb6db1e79cc3d7f6745c6
SarahOtmane <sarahotmane02@gmail.com>
2025-02-25 22:32:41 +0100

    Ajout d'une propriété de style optionnelle au composant ButtonBlack

--- app/src/components/buttons/ButtonBlack.tsx
+++ app/src/components/buttons/ButtonBlack.tsx
@@ -3,11 +3,12 @@ interface ButtonProps {
     text: string;
     icon?: string;
     handleClick?: () => void;
+    style?: string;
 }
 
-const ButtonBlack: React.FC<ButtonProps> = ({text, icon, handleClick}) => {
+const ButtonBlack: React.FC<ButtonProps> = ({text, icon, handleClick, style}) => {
     return (
-        <button onClick={handleClick} className={`flex items-center justify-center px-5 py-3  text-white bg-black rounded-[2rem] hover:text-black hover:bg-white border-black border-1`}>
+        <button onClick={handleClick} className={`flex items-center justify-center px-5 py-3  text-white bg-black rounded-[2rem] hover:text-black hover:bg-white border-black border-1 ${style? style : ''}`}>
             {icon && <img src={icon} alt='icon' className="w-5 h-5 mr-1" />}
             {icon && <span>&nbsp;</span>}
             {text}
