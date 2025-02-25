5220d4c8d0c688f191186eebc23aa356b1993f30
SarahOtmane <sarahotmane02@gmail.com>
2025-02-25 14:41:39 +0100

    Ajout du composant RegisterCard avec des options d'inscription pour les étudiants, entreprises et freelances dans la page d'inscription

--- app/src/pages/Register.tsx
+++ app/src/pages/Register.tsx
@@ -1,5 +1,7 @@
 import PageTitle from "../components/titles/PageTitle";
-import TextUnderTitle from "../components/titles/TextUnderTitle";
+import Pg from "../components/Pg";
+import RegisterCard from "../components/cards/RegisterCard";
+import Icon from "../Icon";
 
 
 const Register: React.FC = () => {
@@ -7,10 +9,30 @@ const Register: React.FC = () => {
         <main className="flex flex-row justify-center h-screen bg-gray-background w-full px-28 pt-50">
             <div className="bg-lilas h-full w-1/3 rounded-">
             </div>
-            <form className="flex flex-col justify-center items-center w-2/3 bg-white">
+            <section className="flex flex-col justify-center items-center w-2/3 bg-white">
                 <PageTitle text='Créer un compte'/>
-                <TextUnderTitle text='Trouvez le professionnel parfait pour répondre à vos besoins quotidiens.'/>
-            </form>
+                <Pg text='Trouvez le professionnel parfait pour répondre à vos besoins quotidiens.'/>
+                <div className="flex flex-row justify-center items-center">
+                    <RegisterCard 
+                        icon={<Icon name="registerStudent" />}
+                        titre='Étudiant' 
+                        description="Crée ton compte étudiant et accède à un portfolio interactif pour mettre en valeur tes compétences." 
+                        selected={true}
+                    />
+                    <RegisterCard
+                        icon={<Icon name="registerCompany" />}
+                        titre='Entreprise'
+                        description="Créez un compte entreprise et publiez vos offres pour attirer les talents adaptés à vos besoins."
+                        selected={false}
+                    />
+                    <RegisterCard
+                        icon={<Icon name="registerSchool" />}
+                        titre='Freelance'
+                        description="Créez un compte freelance et"
+                        selected={false}
+                    />
+                </div>
+            </section>
         </main>
     )
 }
