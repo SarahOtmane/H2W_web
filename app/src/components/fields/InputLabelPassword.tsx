6c37f4e7bcc6ab132e7b29d174c9d187753bf5e8
SarahOtmane <sarahotmane02@gmail.com>
2025-02-25 22:52:59 +0100

    Correction des attributs SVG dans le composant InputLabelPassword

--- app/src/components/fields/InputLabelPassword.tsx
+++ app/src/components/fields/InputLabelPassword.tsx
@@ -16,7 +16,7 @@ const InputLabelPassword: React.FC<InputLabelPasswordProps> = ({label, onChange,
             <div className="relative w-full">
                 <input id="hs-toggle-password" type={type} defaultValue={value} placeholder={placeholder} onChange={onChange} className={`font-Jakarta-regular w-full italic text-sm sm:text-base md:text-lg lg:text-body text-input-text bg-gray-background py-4 pl-4 mt-2 rounded-2xl border-1 border-gray-background focus:outline-lilas`}/>
                 <button type="button" data-hs-toggle-password='{"target": "#hs-toggle-password"}' className="absolute inset-y-0 end-0 flex items-center z-20 px-3 cursor-pointer text-gray-400 rounded-e-md focus:outline-none focus:text-blue-600 dark:text-neutral-600 dark:focus:text-blue-500">
-                    <svg className="shrink-0 size-3.5" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                    <svg className="shrink-0 size-3.5" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
                         <path className="hs-password-active:hidden" d="M9.88 9.88a3 3 0 1 0 4.24 4.24"></path>
                         <path className="hs-password-active:hidden" d="M10.73 5.08A10.43 10.43 0 0 1 12 5c7 0 10 7 10 7a13.16 13.16 0 0 1-1.67 2.68"></path>
                         <path className="hs-password-active:hidden" d="M6.61 6.61A13.526 13.526 0 0 0 2 12s3 7 10 7a9.74 9.74 0 0 0 5.39-1.61"></path>
