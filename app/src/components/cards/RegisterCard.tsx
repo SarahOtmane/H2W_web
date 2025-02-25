3fae757148fe0f5eac540c1a996468636fde25f0
SarahOtmane <sarahotmane02@gmail.com>
2025-02-25 20:57:57 +0100

    Modification du composant RegisterCard pour gérer la sélection via un bouton et mise à jour des propriétés de sélection

--- app/src/components/cards/RegisterCard.tsx
+++ app/src/components/cards/RegisterCard.tsx
@@ -5,23 +5,29 @@ interface RegisterCardProps {
     icon: React.ReactElement;
     titre: string;
     description: string;
-    selected: boolean;
+    selected: string;
+    setSelected: React.Dispatch<React.SetStateAction<string>>;
     style?: string;
+    select: string;
 }
 
-const RegisterCard: React.FC<RegisterCardProps> = ({icon, titre, description, selected, style}) => {
+const RegisterCard: React.FC<RegisterCardProps> = ({icon, titre, description, style, selected, setSelected, select}) => {
+    const registerSelect : boolean = (selected === select ? true : false);
     return (
-        <div className={`
-            flex flex-col w-1/ py-4 px-6 rounded-[1rem] border-1 border-solid h-full
-            ${selected ? 'border-lilas border-2 bg-lilas-light' : 'bg-white border-black'}
-            ${style? style : ''}
-        `}>
+        <button 
+            className={`
+                flex flex-col w-1/ py-4 px-5 rounded-[1rem] border-1 border-solid h-full
+                ${registerSelect ? 'border-lilas border-2 bg-lilas-light' : 'bg-white border-black'}
+                ${style? style : ''}
+            `}
+            onClick={() => setSelected(select)}
+        >
             <article className="flex flex-row items-center">
                 {icon}
                 <p className="font-Jakarta-bold text-sm sm:text-base md:text-lg lg:text-body ml-3">{titre}</p>
             </article>
-            <Pg text={description} style="mt-3"/>
-        </div>
+            <Pg text={description} style="mt-3 text-left"/>
+        </button>
     );
 }
 
