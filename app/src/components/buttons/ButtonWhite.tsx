4a0603eb4537ddb1d3d2217100f8a53f26184d17
SarahOtmane <sarahotmane02@gmail.com>
2025-02-25 22:32:35 +0100

    Modification du composant ButtonWhite pour accepter un élément React comme icône et ajout d'une propriété de style optionnelle

--- app/src/components/buttons/ButtonWhite.tsx
+++ app/src/components/buttons/ButtonWhite.tsx
@@ -1,14 +1,15 @@
 
 interface ButtonProps {
     text: string;
-    icon?: string;
+    icon?: React.ReactElement;
     handleClick?: () => void;
+    style?: string;
 }
 
-const ButtonWhite: React.FC<ButtonProps> = ({text, icon, handleClick}) => {
+const ButtonWhite: React.FC<ButtonProps> = ({text, icon, handleClick, style}) => {
     return (
-        <button onClick={handleClick} className={`flex items-center justify-center px-5 py-3 bg-white border-gray-border border-1  text-black rounded-[2rem] hover:text-white hover:bg-black hover:border-black `}>
-            {icon && <img src={icon} alt='icon' className="w-5 h-5 mr-1" />}
+        <button onClick={handleClick} className={`flex items-center justify-center px-5 py-3 bg-white border-gray-border border-1  text-black rounded-[2rem] hover:text-white hover:bg-black hover:border-black ${style? style : ''}`}>
+            {icon && icon}
             {icon && <span>&nbsp;</span>}
             {text}
         </button>
