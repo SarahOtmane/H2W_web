abcf8a83ed1c7bbb00343b4eaf89f6630f00a801
SarahOtmane <sarahotmane02@gmail.com>
2025-02-26 23:08:01 +0100

    Ajout de la gestion des étapes d'inscription dans le composant RegisterEtape avec des props pour le texte et l'étape d'inscription.

--- app/src/components/RegisterEtape.tsx
+++ app/src/components/RegisterEtape.tsx
@@ -1,12 +1,32 @@
+import Pg from "./Pg";
 
-
-
-const RegisterEtape: React.FC = () => {
-    return(
-        <div>
-
-        </div>
-    )
+interface RegisterEtapeProps {
+    text: string;
+    registerEtape: number;
 }
+
+const RegisterEtape: React.FC<RegisterEtapeProps> = ({ text, registerEtape }) => {
+    return (
+        <div className="flex flex-row w-full mt-12 justify-between text-center items-center relative">
+            <div className="w-1/3 relative flex flex-col items-center">
+                <p className={`font-Jakarta-extra-bold w-max px-4 py-2 rounded-[50%] mb-4 z-10 
+                ${registerEtape === 1 ? 'bg-custom-orange text-white' : 'bg-gray-border text-input-text'}`}>1</p>
+                <Pg text={`Détails de l'${text}`} />
+            </div>
+            {/* <div className="absolute top-[20%] w-1/6 h-[1.5px] bg-gray-border"></div> */}
+            <div className="w-1/3 relative flex flex-col items-center">
+                <p className={`font-Jakarta-extra-bold w-max px-4 py-2 rounded-[50%] mb-4 z-10 
+                ${registerEtape === 2 ? 'bg-custom-orange text-white' : 'bg-gray-border text-input-text'}`}>2</p>
+                <Pg text={`Détails de contact`} />
+            </div>
+            {/* <div className="absolute top-[20%] w-1/6 h-[1.5px] bg-gray-border"></div> */}
+            <div className="w-1/3 relative flex flex-col items-center">
+                <p className={`font-Jakarta-extra-bold w-max px-4 py-2 rounded-[50%] mb-4 z-10 
+                ${registerEtape === 3 ? 'bg-custom-orange text-white' : 'bg-gray-border text-input-text'}`}>3</p>
+                <Pg text={`Vérification`} />
+            </div>
+        </div>
+    );
+};
 
 export default RegisterEtape;
