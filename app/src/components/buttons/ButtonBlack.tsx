5cbb36ed1965216154cb7510e37fc3c046fec9d0
SarahOtmane <sarahotmane02@gmail.com>
2025-02-26 09:20:13 +0100

    Ajout du curseur pointer aux boutons ButtonBlack et ButtonWhite

--- app/src/components/buttons/ButtonBlack.tsx
+++ app/src/components/buttons/ButtonBlack.tsx
@@ -8,7 +8,7 @@ interface ButtonProps {
 
 const ButtonBlack: React.FC<ButtonProps> = ({text, icon, handleClick, style}) => {
     return (
-        <button onClick={handleClick} className={`flex items-center justify-center px-5 py-3  text-white bg-black rounded-[2rem] hover:text-black hover:bg-white border-black border-1 ${style? style : ''}`}>
+        <button onClick={handleClick} className={`flex items-center justify-center px-5 py-3 cursor-pointer text-white bg-black rounded-[2rem] hover:text-black hover:bg-white border-black border-1 ${style? style : ''}`}>
             {icon && <img src={icon} alt='icon' className="w-5 h-5 mr-1" />}
             {icon && <span>&nbsp;</span>}
             {text}
--- app/src/components/buttons/ButtonWhite.tsx
+++ app/src/components/buttons/ButtonWhite.tsx
@@ -8,7 +8,7 @@ interface ButtonProps {
 
 const ButtonWhite: React.FC<ButtonProps> = ({text, icon, handleClick, style}) => {
     return (
-        <button onClick={handleClick} className={`flex items-center justify-center px-5 py-3 bg-white border-gray-border border-1  text-black rounded-[2rem] hover:text-white hover:bg-black hover:border-black ${style? style : ''}`}>
+        <button onClick={handleClick} className={`flex items-center justify-center px-5 py-3 cursor-pointer bg-white border-gray-border border-1  text-black rounded-[2rem] hover:text-white hover:bg-black hover:border-black ${style? style : ''}`}>
             {icon && icon}
             {icon && <span>&nbsp;</span>}
             {text}
