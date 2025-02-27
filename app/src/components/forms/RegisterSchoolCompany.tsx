2b8a1ce8599da0a4e23ca9ec3a9a7677878972cc
SarahOtmane <sarahotmane02@gmail.com>
2025-02-27 14:59:17 +0100

    Ajout de la gestion de l'étape d'inscription 3 pour la confirmation de l'adresse email dans le composant RegisterSchoolCompany.

--- app/src/components/forms/RegisterSchoolCompany.tsx
+++ app/src/components/forms/RegisterSchoolCompany.tsx
@@ -1,3 +1,4 @@
+import Subtitle from "../titles/Subtitle";
 import RegisterEtape1 from "./RegisterEtape1";
 import RegisterEtape2 from "./RegisterEtape2";
 
@@ -36,6 +37,12 @@ const RegisterSchoolCompany: React.FC<RegisterSchoolCompanyProps> = ({select, in
 
             {  registerEtape === 2 &&
                 <RegisterEtape2  information={information} setInformation={setInformation} setRegisterEtape={setRegisterEtape} />
+            }
+            {  registerEtape === 3 &&
+                <div className="flex flex-col mt-14 text-center w-full px-28">
+                    <Subtitle text="Confirmez votre adresse email" />
+                    <p className="mt-4">Nous avons envoyé un email de confirmation à [{information.email}]. <br></br>Cliquez sur le lien dans l’email pour activer votre compte et commencer votre recherche d’alternance.</p>
+                </div>
             }
         </>
     )
