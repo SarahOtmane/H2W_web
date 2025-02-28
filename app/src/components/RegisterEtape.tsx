3b2271ddb71f4999d7541a259cf7ea576187aba2
SarahOtmane <sarahotmane02@gmail.com>
2025-02-28 09:38:34 +0100

    Réorganisation de la mise en page du composant RegisterEtape pour améliorer l'affichage des étapes d'inscription.

--- app/src/components/RegisterEtape.tsx
+++ app/src/components/RegisterEtape.tsx
@@ -7,23 +7,25 @@ interface RegisterEtapeProps {
 
 const RegisterEtape: React.FC<RegisterEtapeProps> = ({ text, registerEtape }) => {
     return (
-        <div className="flex flex-row w-full mt-12 justify-between text-center items-center relative">
-            <div className="w-1/3 relative flex flex-col items-center">
-                <p className={`font-Jakarta-extra-bold w-max px-4 py-2 rounded-[50%] mb-4 z-10 
-                ${registerEtape === 1 ? 'bg-custom-orange text-white' : 'bg-gray-border text-input-text'}`}>1</p>
-                <Pg text={`Détails de l'${text}`} />
-            </div>
-            {/* <div className="absolute top-[20%] w-1/6 h-[1.5px] bg-gray-border"></div> */}
-            <div className="w-1/3 relative flex flex-col items-center">
-                <p className={`font-Jakarta-extra-bold w-max px-4 py-2 rounded-[50%] mb-4 z-10 
-                ${registerEtape === 2 ? 'bg-custom-orange text-white' : 'bg-gray-border text-input-text'}`}>2</p>
-                <Pg text={`Détails de contact`} />
-            </div>
-            {/* <div className="absolute top-[20%] w-1/6 h-[1.5px] bg-gray-border"></div> */}
-            <div className="w-1/3 relative flex flex-col items-center">
-                <p className={`font-Jakarta-extra-bold w-max px-4 py-2 rounded-[50%] mb-4 z-10 
-                ${registerEtape === 3 ? 'bg-custom-orange text-white' : 'bg-gray-border text-input-text'}`}>3</p>
-                <Pg text={`Vérification`} />
+        <div className="flex flex-col w-full mt-12 text-center items-center">
+            <div className="flex items-center justify-between mb-4 w-2/3">
+                <div className="relative">
+                    <p className={`font-Jakarta-extra-bold w-max px-4 py-2 rounded-[50%] z-10 
+                    ${registerEtape === 1 ? 'bg-custom-orange text-white' : 'bg-gray-border text-input-text'}`}>1</p>
+                    <Pg text={`Détails de l'${text}`} style="absolute w-max" />
+                </div>
+                <div className="w-1/3 h-[1.5px] bg-gray-border mx-4"></div>
+               <div className="relative">
+                <p className={`font-Jakarta-extra-bold w-max px-4 py-2 rounded-[50%] z-10 
+                 ${registerEtape === 2 ? 'bg-custom-orange text-white' : 'bg-gray-border text-input-text'}`}>2</p>
+                <Pg text={`Détails de contact`} style="absolute w-max" />
+               </div>
+                <div className="w-1/3 h-[1.5px] bg-gray-border mx-4"></div>
+                <div className="relative">
+                    <p className={`font-Jakarta-extra-bold w-max px-4 py-2 rounded-[50%] z-10 
+                    ${registerEtape === 3 ? 'bg-custom-orange text-white' : 'bg-gray-border text-input-text'}`}>3</p>
+                    <Pg text={`Vérification`} style="absolute w-max" />
+                </div>
             </div>
         </div>
     );
