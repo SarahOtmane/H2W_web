29898a63356a37cffb25f8d502e8ead63e46c568
SarahOtmane <sarahotmane02@gmail.com>
2025-02-28 10:11:23 +0100

    Ajout de la prop 'required' dans plusieurs composants InputLabel pour renforcer la validation des champs de saisie dans les formulaires d'inscription.

--- app/src/components/forms/RegisterEtape1.tsx
+++ app/src/components/forms/RegisterEtape1.tsx
@@ -37,22 +37,22 @@ const RegisterEtape1: React.FC<RegisterEtape1Props> = ({select, information, set
     return(
         <form className="flex flex-col w-full mt-14">
             <div className="flex flex-row justify-between w-full">
-                <InputLabel name={`${select==='entreprise' ? "nomEntreprise" : "nomEcole"}`} type='text' style={`w-1/2 ${select==='entreprise' ? "mr-4" : "w-full"}`} label={`${select==='entreprise' ? "Nom de l'entreprise" : "Nom de l'école"}`} placeholder="ex : WebTech Solutions" value={select==='entreprise' ? information.nomEntreprise : information.nomEcole} onChange={handleChange}/>
+                <InputLabel required name={`${select==='entreprise' ? "nomEntreprise" : "nomEcole"}`} type='text' style={`w-1/2 ${select==='entreprise' ? "mr-4" : "w-full"}`} label={`${select==='entreprise' ? "Nom de l'entreprise" : "Nom de l'école"}`} placeholder="ex : WebTech Solutions" value={select==='entreprise' ? information.nomEntreprise : information.nomEcole} onChange={handleChange}/>
                 {select==='entreprise' &&
                     <InputLabel name="secteurActivite" type='text' style='w-1/2 ml-4' label="Secteur d'acitivité" placeholder="ex : Informatique, Marketing, ..." value={information.secteurActivite} onChange={handleChange}/>
                 }
             </div>
             <div className="flex flex-row justify-between w-full mt-5">
-                <InputLabel name="adresse" type='text' style={`w-1/2 ${select==='entreprise' ? "mr-4" : "w-full"}`} label={`${select==='entreprise' ? "Adresse de l'entreprise" : "Adresse de l'école"}`} placeholder="ex : 15 rue des Entrepreneurs" value={information.adresse} onChange={handleChange}/>
+                <InputLabel required name="adresse" type='text' style={`w-1/2 ${select==='entreprise' ? "mr-4" : "w-full"}`} label={`${select==='entreprise' ? "Adresse de l'entreprise" : "Adresse de l'école"}`} placeholder="ex : 15 rue des Entrepreneurs" value={information.adresse} onChange={handleChange}/>
                 {select==='entreprise' &&
                     <InputLabel name="complementAdresse" type='text' style='w-1/2 ml-4' label="Complément de l’adresse" placeholder="ex : Bâtiment B, 3e étage (optionnel)" value={information.complementAdresse} onChange={handleChange}/>
                 }
             </div>
             <div className="flex flex-row justify-between w-full mt-5">
-                <InputLabel name="ville" type='text' style='w-1/2 mr-4' label="Ville" placeholder="ex : Paris" value={information.ville} onChange={handleChange}/>
-                <InputLabel name="codePostal" type='text' style='w-1/2 ml-4' label="Code postal" placeholder="ex : 75002" value={information.codePostal} onChange={handleChange}/>
+                <InputLabel required name="ville" type='text' style='w-1/2 mr-4' label="Ville" placeholder="ex : Paris" value={information.ville} onChange={handleChange}/>
+                <InputLabel required name="codePostal" type='text' style='w-1/2 ml-4' label="Code postal" placeholder="ex : 75002" value={information.codePostal} onChange={handleChange}/>
             </div>
-            <InputLabel name="siret" type='text' style='w-1/2 w-full mt-5 mb-14' label="Numéro de SIRET" placeholder="ex : 123 456 789 00012" value={information.siret} onChange={handleChange}/>
+            <InputLabel required name="siret" type='text' style='w-1/2 w-full mt-5 mb-14' label="Numéro de SIRET" placeholder="ex : 123 456 789 00012" value={information.siret} onChange={handleChange}/>
             <ButtonBlack text="Suivant" style="py-5" handleClick={() => setRegisterEtape(2)} />
             <p className="text-center mt-7">Vous avez déjà un compte ? <a href='#' className="text-lilas">Se connecter</a></p>
         </form>
--- app/src/components/forms/RegisterStudent.tsx
+++ app/src/components/forms/RegisterStudent.tsx
@@ -37,19 +37,19 @@ const RegisterStudent: React.FC<RegisterStudentProps> = ({information, setInform
     return(
         <form className="flex flex-col w-full">
             <div className="flex flex-row justify-between w-full mt-10">
-                <InputLabel name="prenom" type='text' style='w-1/2 mr-4' label="Prénom" placeholder="ex: Thomas" value={information.prenom} onChange={handleChange}/>
-                <InputLabel name="nom" type='text' style='w-1/2 ml-4' label="Nom" placeholder="ex: Durant" value={information.nom} onChange={handleChange}/>
+                <InputLabel required name="prenom" type='text' style='w-1/2 mr-4' label="Prénom" placeholder="ex: Thomas" value={information.prenom} onChange={handleChange}/>
+                <InputLabel required name="nom" type='text' style='w-1/2 ml-4' label="Nom" placeholder="ex: Durant" value={information.nom} onChange={handleChange}/>
             </div>
             <div className="flex flex-row justify-between w-full mt-5">
-                <InputLabel name="email" type='email' style='w-1/2 mr-4' label="E-mail" placeholder="ex: thomas.durant@email.com" value={information.email} onChange={handleChange}/>
-                <InputLabel name="numero" type='tel' style='w-1/2 ml-4' label="Numéro de téléphone" placeholder="ex: 0612345678" value={information.numero} onChange={handleChange}/>
+                <InputLabel required name="email" type='email' style='w-1/2 mr-4' label="E-mail" placeholder="ex: thomas.durant@email.com" value={information.email} onChange={handleChange}/>
+                <InputLabel required name="numero" type='tel' style='w-1/2 ml-4' label="Numéro de téléphone" placeholder="ex: 0612345678" value={information.numero} onChange={handleChange}/>
             </div>
-            <InputLabelPassword name="password" style='w-full mt-5' label="Mot de passe" placeholder="********" value={information.motDePasse} onChange={handleChange}/>
+            <InputLabelPassword required name="password" style='w-full mt-5' label="Mot de passe" placeholder="********" value={information.motDePasse} onChange={handleChange}/>
             <div className="flex flex-row items-center mt-1">
                 <Icon name="information"/>
                 <p className="text-input-text ml-1 italic">Doit contenir au moins 8 caractères, 1 majuscule, 1 minuscule, 1 chiffre et 1 caractère spécial</p>
             </div>
-            <InputLabelPassword name="confirmationMotDePasse" style='w-full mt-5' label="Confirmation du mot de passe" placeholder="Entrez le mot de passe à nouveau" value={information.confirmationMotDePasse} onChange={handleChange}/>
+            <InputLabelPassword required name="confirmationMotDePasse" style='w-full mt-5' label="Confirmation du mot de passe" placeholder="Entrez le mot de passe à nouveau" value={information.confirmationMotDePasse} onChange={handleChange}/>
             <div className="flex flex-row items-center mt-5 pl-2 mb-10">
                 <input type="checkbox" className="w-5 h-5 accent-lilas" />
                 <p className="font-Jakarta-medium text-[15px] ml-2">
