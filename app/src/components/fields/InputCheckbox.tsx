ba50a7ba1f924e3d80673749e1c4b451c73ee5ff
SarahOtmane <sarahotmane02@gmail.com>
2025-03-01 22:20:36 +0100

    Ajout du composant InputCheckbox pour gérer les cases à cocher dans l'application.

---
+++ app/src/components/fields/InputCheckbox.tsx
@@ -0,0 +1,15 @@
+
+
+
+
+const InputCheckbox: React.FC = () =>{
+
+    return(
+        <input 
+            type="checkbox" 
+            className="w-5 h-5 rounded-[1rem] border border-gray-dark cursor-pointer accent-lilas"
+        />
+    )
+}
+
+export default InputCheckbox;
