8b484ee12535c37df0c659db4a655abfbcab9691
SarahOtmane <sarahotmane02@gmail.com>
2025-03-01 22:54:11 +0100

    Suppression de la propriété 'required' du composant InputLabel pour simplifier les props

--- app/src/components/fields/InputLabel.tsx
+++ app/src/components/fields/InputLabel.tsx
@@ -9,11 +9,10 @@ interface InputLabelProps {
     type: string;
     style?: string;
     name: string;
-    required?: boolean;
     error?: string;
 }
 
-const InputLabel: React.FC<InputLabelProps> = ({label, onChange, value, placeholder, type, style, name, required, error}) => {
+const InputLabel: React.FC<InputLabelProps> = ({label, onChange, value, placeholder, type, style, name, error}) => {
     return (
         <div className={`flex flex-col ${style? style : ''}`}>
             <label className="font-Jakarta-medium text-sm sm:text-base md:text-lg lg:text-body ml-1">{label}</label>
@@ -23,7 +22,6 @@ const InputLabel: React.FC<InputLabelProps> = ({label, onChange, value, placehol
                 placeholder={placeholder} 
                 onChange={onChange} 
                 name={name}
-                required={required}
                 className={`font-Jakarta-regular italic text-sm sm:text-base md:text-lg lg:text-body  py-4 pl-4 mt-2 rounded-2xl border-1 ${error ? 'text-custom-red border-custom-red bg-red-background' : 'text-input-text bg-gray-background border-gray-background focus:outline-lilas'} `}
             />
             {error &&
