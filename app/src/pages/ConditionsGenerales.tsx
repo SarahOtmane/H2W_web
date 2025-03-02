7155538d85e112554c6b1a3019e5cdb1ea0ed074
SarahOtmane <sarahotmane02@gmail.com>
2025-03-02 22:12:33 +0100

    Ajout de la page Conditions Générales avec le composant LiensPagesLegales

---
+++ app/src/pages/ConditionsGenerales.tsx
@@ -0,0 +1,12 @@
+import LiensPagesLegales from "../components/LiensPagesLegales";
+
+
+const ConditionsGenerales = () => {
+    return (
+        <div className=" bg-gray-background w-full px-34 pt-10 pb-20">
+            <LiensPagesLegales text="Conditions Générales" link="cgu" />
+        </div>
+    );
+}
+    
+export default ConditionsGenerales;
