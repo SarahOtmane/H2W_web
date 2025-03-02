3933cbaaa1d92e5d256b88b1651189355d5543f8
SarahOtmane <sarahotmane02@gmail.com>
2025-03-02 22:12:42 +0100

    Ajout de la page Politique de Confidentialité avec le composant LiensPagesLegales

---
+++ app/src/pages/PolitiqueConfidentialité.tsx
@@ -0,0 +1,12 @@
+import LiensPagesLegales from "../components/LiensPagesLegales";
+
+
+const PolitiqueConfidentialite = () => {
+    return (
+        <div className=" bg-gray-background w-full px-34 pt-10 pb-20">
+            <LiensPagesLegales text="Politique de Confidentialité et RGPD" link="plc" />
+        </div>
+    );
+}
+    
+export default PolitiqueConfidentialite;
