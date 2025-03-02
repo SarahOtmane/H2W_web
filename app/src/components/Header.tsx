ee28afc4e3a65e7806153a7fd154b70490aaad05
SarahOtmane <sarahotmane02@gmail.com>
2025-03-02 20:38:10 +0100

    Ajout du logo et des liens de navigation dans le composant Header, intégration du bouton de connexion avec icône

--- app/src/components/Header.tsx
+++ app/src/components/Header.tsx
@@ -1,9 +1,20 @@
+import logo from '../assets/images/logo_header.png';
+import Icon from '../Icon';
 
+import ButtonBlack from './buttons/ButtonBlack';
 
 const Header = () => {
     return (
-        <header>
-            <h1>My App</h1>
+        <header className='flex flex-row justify-between items-center bg-gray-background px-38 py-8'>
+            <img src={logo} alt='logo' />
+            <ul className='flex flex-row font-Jakarta-semi-bold'>
+                <li className='mr-8'><a href='#'>Nos services</a></li>
+                <li className='mr-8'><a href='#'>Étudiant</a></li>
+                <li className='mr-8'><a href='#'>Entreprise</a></li>
+                <li className='mr-8'><a href='#'>École</a></li>
+                <li className='mr-8'><a href='#'>À propos</a></li>
+            </ul>
+            <ButtonBlack text='  Se connecter' icon={<Icon name='avatar' />} />
         </header>
     );
 }
