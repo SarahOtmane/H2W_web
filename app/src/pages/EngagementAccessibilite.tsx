aa0362c84c37b380802c4d6689a0e0a4955bef56
SarahOtmane <sarahotmane02@gmail.com>
2025-03-02 22:12:38 +0100

    Ajout de la page Engagement Accessibilité avec le composant LiensPagesLegales

---
+++ app/src/pages/EngagementAccessibilite.tsx
@@ -0,0 +1,12 @@
+import LiensPagesLegales from "../components/LiensPagesLegales";
+
+
+const EngagementAccessibilite = () => {
+    return (
+        <div className=" bg-gray-background w-full px-34 pt-10 pb-20">
+            <LiensPagesLegales text="Engagements en matière d’Accessibilité" link="acc" />
+        </div>
+    );
+}
+    
+export default EngagementAccessibilite;
