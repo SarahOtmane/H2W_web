6ad79b3624b51bb5f1d3a4d6944de9404fe393c4
SarahOtmane <sarahotmane02@gmail.com>
2025-03-02 22:58:23 +0100

    Ajout de la navigation vers la page de connexion dans le composant Header

--- app/src/components/Header.tsx
+++ app/src/components/Header.tsx
@@ -1,10 +1,11 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import logo from '../assets/images/logo_header.png';
 import Icon from '../Icon';
 
 import ButtonBlack from './buttons/ButtonBlack';
 
 const Header = () => {
+    const navigate = useNavigate();
     return (
         <header className='flex flex-row justify-between items-center bg-gray-background px-38 py-8'>
             <img src={logo} alt='logo' />
@@ -15,7 +16,7 @@ const Header = () => {
                 <li className='mr-8'><Link to='#'>École</Link></li>
                 <li className='mr-8'><Link to='#'>À propos</Link></li>
             </ul>
-            <ButtonBlack text='  Se connecter' icon={<Icon name='avatar' />} />
+            <ButtonBlack text='  Se connecter' icon={<Icon name='avatar' />} handleClick={()=>navigate('/login')} />
         </header>
     );
 }
