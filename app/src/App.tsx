2a8881f11207edb77c98e6ec5f08ae40ed2b9962
SarahOtmane <sarahotmane02@gmail.com>
2025-03-02 20:39:39 +0100

    Ajout de la route pour le composant Login dans l'application

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -6,6 +6,7 @@ import Home from './pages/Home';
 import Register from './pages/Register';
 import Footer from './components/Footer';
 import Header from './components/Header';
+import Login from './pages/Login';
 
 const App: React.FC = () => {
     return (
@@ -14,6 +15,7 @@ const App: React.FC = () => {
             <Routes>
                 <Route path='/' element={<Home />} />
                 <Route path='/register' element={<Register />} />
+                <Route path='/login' element={<Login />} />
             </Routes>
             <Footer />
         </Router>
