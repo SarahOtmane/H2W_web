d53fd3969a4ff16de8e7c596a58c77eb7e27cd72
SarahOtmane <sarahotmane02@gmail.com>
2025-03-02 19:18:55 +0100

    Ajout du composant Header dans le fichier App.tsx

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -5,10 +5,12 @@ import './App.css';
 import Home from './pages/Home';
 import Register from './pages/Register';
 import Footer from './components/Footer';
+import Header from './components/Header';
 
 const App: React.FC = () => {
     return (
         <Router>
+            <Header />
             <Routes>
                 <Route path='/' element={<Home />} />
                 <Route path='/register' element={<Register />} />
