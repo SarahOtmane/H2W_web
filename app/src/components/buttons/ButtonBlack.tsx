8ba47e76c2e0618a0152012af8389dbf9702102d
SarahOtmane <sarahotmane02@gmail.com>
2025-03-02 20:33:54 +0100

    Modification des composants ButtonBlack et ButtonWhite pour accepter des éléments React comme icônes et ajustement de l'affichage du texte.

--- app/src/components/buttons/ButtonBlack.tsx
+++ app/src/components/buttons/ButtonBlack.tsx
@@ -1,7 +1,7 @@
 
 interface ButtonProps {
     text: string;
-    icon?: string;
+    icon?: React.ReactElement;
     handleClick?: (e: React.MouseEvent<HTMLButtonElement>) => void;
     style?: string;
 }
@@ -9,9 +9,9 @@ interface ButtonProps {
 const ButtonBlack: React.FC<ButtonProps> = ({text, icon, handleClick, style}) => {
     return (
         <button onClick={handleClick} className={`flex items-center justify-center px-5 py-3 cursor-pointer text-white bg-black rounded-[2rem] hover:text-black hover:bg-white border-black border-1 ${style? style : ''}`}>
-            {icon && <img src={icon} alt='icon' className="w-5 h-5 mr-1" />}
-            {icon && <span>&nbsp;</span>}
-            {text}
+            {icon && icon}
+            {icon && <span> &nbsp; </span>}
+            <p className={icon ? 'ml-1.5' : ''}>{text}</p>
         </button>
     );
 };
