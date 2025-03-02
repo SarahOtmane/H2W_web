e493e293be3cbe55abea7276da330a9d066ff9ba
SarahOtmane <sarahotmane02@gmail.com>
2025-03-02 20:53:20 +0100

    Suppression des composants inutilisés et nettoyage du code dans la page d'accueil

--- app/src/pages/Home.tsx
+++ app/src/pages/Home.tsx
@@ -1,44 +1,10 @@
-import { useState } from "react";
-
-import '../App.css';
-import icon from "../assets/react.svg"
-
-import ButtonBlack from "../components/buttons/ButtonBlack";
-import ButtonWhite from "../components/buttons/ButtonWhite";
-import SelectEspace from "../components/fields/SelectEspace";
-import PageTitle from "../components/titles/PageTitle";
-import SectionTitle from "../components/titles/SectionTitle";
-import Subtitle from "../components/titles/Subtitle";
-import Pg from "../components/Pg";
-import InputLabel from "../components/fields/InputLabel";
-import SearchBar from "../components/SearchBar";
 
 
 const Home: React.FC = () => {
-    const [espaceSelected, setEspaceSelected] = useState<string>('Espace étudiant');
 
     return (
         <div className="ml-10">
-            <h1>Home</h1>
-            <ButtonBlack
-                text='Click me'
-                icon={icon}
-            />
-            <ButtonWhite
-                text='Click me'
-                icon={icon}
-            />
-            <SelectEspace  espaceSelected={espaceSelected} setEspaceSelected={setEspaceSelected}/>
-            <PageTitle text='Page Title'/>
-            <SectionTitle text='Section Title'/>
-            <Subtitle text='Subtitle'/>
-            <Pg text='Text' />
-
-            <div className="w-1/5">
-                <InputLabel label='Label' value='' placeholder="test" setValue={() => {}}/>
-            </div>
-
-            <SearchBar metier='' localisation='' setLocalisation={() => {}} setMetier={() => {}}/>
+            
         </div>
     );
 }
