863ae84fb4bd45c7ce40796e69c4c3242dcbe34a
SarahOtmane <sarahotmane02@gmail.com>
2025-03-08 01:33:43 +0100

    Ajout des sections Section1, Etapes, Section2, Abonnements et Newsletter dans la page d'accueil

--- app/src/pages/Home.tsx
+++ app/src/pages/Home.tsx
@@ -1,10 +1,19 @@
+import Abonnements from "../components/home/Abonnements";
+import Etapes from "../components/home/Etapes";
+import Section1 from "../components/home/section1";
+import Section2 from "../components/home/Section2";
+import Newsletter from "../components/Newsletter";
 
 
 const Home: React.FC = () => {
 
     return (
-        <div className="ml-10">
-            
+        <div className=" bg-gray-background w-full px-38 pt-10 pb-20">
+            <Section1 />
+            <Etapes />
+            <Section2 />
+            <Abonnements />
+            <Newsletter />
         </div>
     );
 }
