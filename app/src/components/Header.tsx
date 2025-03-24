745b206ea81e59f3a2f39a705154fdf039bd829b
SarahOtmane <sarahotmane02@gmail.com>
2025-03-24 10:46:29 +0100

    Ajout de la gestion de la taille de la fenêtre dans le composant Header pour afficher le logo approprié selon la taille de l'écran

--- app/src/components/Header.tsx
+++ app/src/components/Header.tsx
@@ -2,12 +2,17 @@ import { Link, useNavigate } from 'react-router-dom';
 import Icon from '../utils/Icon';
 
 import ButtonBlack from './buttons/ButtonBlack';
+import WindowSize from '../utils/WindowSize';
 
 const Header = () => {
     const navigate = useNavigate();
+
+    const size = WindowSize();
+    const isMobile = size.width < 768;
+
     return (
         <header className='flex flex-row justify-between items-center bg-gray-background px-4 py-1 md:px-16 md:py-3 lg:px-38 lg:py-8'>
-            <Icon name='logoHeader' />
+            <Icon name={isMobile ? 'logoHeaderMobile' : 'logoHeaderMobile'} />
             <ul className='md:flex md:flex-row font-Jakarta-semi-bold hidden'>
                 <li className='mr-8'><Link to='#'>Nos services</Link></li>
                 <li className='mr-8'><Link to='#'>Étudiant</Link></li>
