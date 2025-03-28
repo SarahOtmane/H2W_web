6e602a84bbd97722c072cdf0ca9bd20bd32fe41f
SarahOtmane <sarahotmane02@gmail.com>
2025-03-29 00:23:41 +0100

    Refactor le composant App pour conditionner l'affichage de l'en-tête selon la route

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 
 import './App.css';
 import Home from './pages/Home';
@@ -12,24 +12,39 @@ import PolitiqueConfidentialite from './pages/PolitiqueConfidentialité';
 import EngagementAccessibilite from './pages/EngagementAccessibilite';
 import LandingPage from './pages/LandingPage';
 
-const App: React.FC = () => {
+const AppContent: React.FC = () => {
+    const location = useLocation();
+
+    const renderHeader = () => {
+        if (location.pathname === '/landing-page') {
+            return null; 
+        }
+        return <Header />; 
+    };
+
     return (
-        <Router>
-            <Header />
+        <>
+            {renderHeader()}
             <Routes>
                 <Route path='/' element={<Home />} />
                 <Route path='/register' element={<Register />} />
                 <Route path='/login' element={<Login />} />
                 <Route path='/landing-page' element={<LandingPage />} />
-
                 <Route path='/conditions-generales' element={<ConditionsGenerales />} />
                 <Route path='/politique-confidentialite' element={<PolitiqueConfidentialite />} />
                 <Route path='/engagement-accessibilite' element={<EngagementAccessibilite />} />
-
             </Routes>
             <Footer />
+        </>
+    );
+};
+
+const App: React.FC = () => {
+    return (
+        <Router>
+            <AppContent />
         </Router>
     );
-}
+};
 
 export default App;
