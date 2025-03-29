5fe72d961218de978cac9e4e1d0f2af17b782b4a
SarahOtmane <sarahotmane02@gmail.com>
2025-03-29 01:20:08 +0100

    Refactor le composant App pour afficher la page d'accueil sur la route principale et masquer l'en-tête

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -2,21 +2,15 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 
 import './App.css';
-import Home from './pages/Home';
-import Register from './pages/Register';
 import Footer from './components/Footer';
 import Header from './components/Header';
-import Login from './pages/Login';
-import ConditionsGenerales from './pages/ConditionsGenerales';
-import PolitiqueConfidentialite from './pages/PolitiqueConfidentialité';
-import EngagementAccessibilite from './pages/EngagementAccessibilite';
 import LandingPage from './pages/LandingPage';
 
 const AppContent: React.FC = () => {
     const location = useLocation();
 
     const renderHeader = () => {
-        if (location.pathname === '/landing-page') {
+        if (location.pathname === '/') {
             return null; 
         }
         return <Header />; 
@@ -26,13 +20,7 @@ const AppContent: React.FC = () => {
         <>
             {renderHeader()}
             <Routes>
-                <Route path='/' element={<Home />} />
-                <Route path='/register' element={<Register />} />
-                <Route path='/login' element={<Login />} />
-                <Route path='/landing-page' element={<LandingPage />} />
-                <Route path='/conditions-generales' element={<ConditionsGenerales />} />
-                <Route path='/politique-confidentialite' element={<PolitiqueConfidentialite />} />
-                <Route path='/engagement-accessibilite' element={<EngagementAccessibilite />} />
+                <Route path='/' element={<LandingPage />} />
             </Routes>
             <Footer />
         </>
