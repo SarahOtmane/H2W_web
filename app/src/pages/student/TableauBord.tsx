99b59104a9954e9224b925e75ef4dac0bbb4c8ff
SarahOtmane <sarahotmane02@gmail.com>
2025-03-29 20:56:20 +0100

    Ajout d'une nouvelle image SVG pour le fond de la section Contact et mise à jour du style de la section Newsletter dans le tableau de bord étudiant.

--- app/src/pages/student/TableauBord.tsx
+++ app/src/pages/student/TableauBord.tsx
@@ -1,10 +1,11 @@
 import MenuItem from "../../components/student/MenuItem";
 import TitrePage from "../../components/student/TitrePage";
-
-import back from '../../assets/images/student/tbord_newsletter_back.png';
 import ButtonWhite from "../../components/buttons/ButtonWhite";
 import Icon from "../../utils/Icon";
+import ButtonBlack from "../../components/buttons/ButtonBlack";
 
+import backN from '../../assets/images/student/tbord_newsletter_back.png';
+import backC from '../../assets/images/student/tbord_contact_back.svg';
 
 
 
@@ -15,8 +16,8 @@ const TableauBord = () => {
             <TitrePage title="Tableau de bord" />
 
             <section className="flex flex-col md:flex-row justify-between mt-18">
-                <div className="w-1/2 ml-0 md:ml-[.5rem] bg-custom-black text-white rounded-[1rem] pl-12 py-10"
-                    style={{ backgroundImage: `url(${back})`, backgroundRepeat: 'no-repeat',backgroundPosition: 'right top',}}>
+                <div className="w-1/2 mr-0 md:mr-[.7rem] bg-custom-black text-white rounded-[1rem] pl-12 py-10"
+                    style={{ backgroundImage: `url(${backN})`, backgroundRepeat: 'no-repeat',backgroundPosition: 'right top',}}>
                     <h2 className="font-Jakarta-extra-bold text-[36px] w-[80%] ">Newsletter Hive2Work</h2>
                     <p className="w-[75%] text-input-text text-[18px] my-6 font-Jakarta-medium">Suivez les dernières actualités de votre plateforme de portfolios interactifs.</p>
                     <span className="text-[18px] font-Jakarta-bold">E-mail*</span>
@@ -30,6 +31,12 @@ const TableauBord = () => {
                         </div>
                         <ButtonWhite text="S'abonner" icon={<Icon name="flecheButton" />} style="mt-1"/> 
                     </div>
+                </div>
+                <div className="w-1/2 bg-white rounded-[1rem] pl-12 py-10 flex flex-col ml-0 md:ml-[.7rem]"
+                    style={{ backgroundImage: `url(${backC})`, backgroundRepeat: 'no-repeat',backgroundPosition: 'right bottom',}}>
+                    <h2 className="font-Jakarta-extra-bold text-[36px] w-[80%] ">Contacter Hive2Work</h2>
+                    <p className="w-[75%] text-input-text text-[18px] my-6 font-Jakarta-medium">Vous avez un problème ou une question ? <br></br>Contactez nous !</p>
+                    <ButtonBlack text="Contacter" icon={<Icon name="flecheButton" />} style="w-max mt-5" /> 
                 </div>
             </section>
         </main>
