1332731bdaff58f526ea174deb3a6d25d20496ba
SarahOtmane <sarahotmane02@gmail.com>
2025-04-12 22:51:15 +0200

    Adjust padding and background position in TitrePage component for improved layout

--- app/src/components/student/TitrePage.tsx
+++ app/src/components/student/TitrePage.tsx
@@ -8,7 +8,7 @@ interface TitrePageProps {
 
 const TitrePage : React.FC<TitrePageProps> = ({title}) => {
     return(
-        <div className='bg-lilas text-white py-10 rounded-[1rem] mt-[-.5rem]' style={{ backgroundImage: `url(${back})`, backgroundRepeat: 'no-repeat', backgroundPosition: '95% 70%' }}>
+        <div className='bg-lilas text-white pt-12 pb-20 rounded-[1rem] mt-[-.5rem]' style={{ backgroundImage: `url(${back})`, backgroundRepeat: 'no-repeat', backgroundPosition: '85% 70%' }}>
             <h1 className='font-Jakarta-bold text-[36px] text-center'>{title}</h1>
         </div>
     )
