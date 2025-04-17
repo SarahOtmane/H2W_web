eb836373f99fb66c2909286a5d1f42a4344347af
SarahOtmane <sarahotmane02@gmail.com>
2025-04-17 23:13:53 +0200

    Ajout du composant CreatePortfolioButton pour la navigation entre les étapes du portfolio.

---
+++ app/src/components/buttons/CreatePortfolioButton.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+interface CreatePortfolioButtonProps {
+    suivantSelected: boolean;
+    setEtape: (etape: number) => void;
+    etape: number;
+}
+
+
+const CreatePortfolioButton  : React.FC<CreatePortfolioButtonProps> = ({suivantSelected, setEtape, etape}) => {
+    return(
+        <div className="flex flex-row justify-center items-center mt-15">
+            <button className="text-[16px] py-4 w-[9rem] rounded-[2rem] text-white bg-custom-black mr-4" onClick={(e) => { e.preventDefault(); setEtape(etape - 1); }}>Précédent</button>
+            <button className={`text-[16px] py-4 w-[9rem] rounded-[2rem] text-white ${suivantSelected ? 'bg-custom-orange' : 'bg-[#9FA6B2]'}`} onClick={(e) => { e.preventDefault(); setEtape(etape + 1); }} >Suivant</button>
+        </div>
+    )
+}
+
+export default CreatePortfolioButton;
