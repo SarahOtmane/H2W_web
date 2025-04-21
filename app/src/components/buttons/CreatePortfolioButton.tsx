e83276fae9eb1d661e2febff5b5c9a5d0ce5fd59
SarahOtmane <sarahotmane02@gmail.com>
2025-04-21 19:21:56 +0200

    Correction des classes CSS pour les boutons Précédent et Suivant dans le composant CreatePortfolioButton.

--- app/src/components/buttons/CreatePortfolioButton.tsx
+++ app/src/components/buttons/CreatePortfolioButton.tsx
@@ -10,8 +10,8 @@ interface CreatePortfolioButtonProps {
 const CreatePortfolioButton  : React.FC<CreatePortfolioButtonProps> = ({suivantSelected, setEtape, etape}) => {
     return(
         <div className="flex flex-row justify-center items-center mt-15">
-            <button className="text-[16px] py-4 w-[9rem] rounded-[2rem] text-white bg-custom-black mr-4" onClick={(e) => { e.preventDefault(); setEtape(etape - 1); }}>Précédent</button>
-            <button className={`text-[16px] py-4 w-[9rem] rounded-[2rem] text-white ${suivantSelected ? 'bg-custom-orange' : 'bg-[#9FA6B2]'}`} onClick={(e) => { e.preventDefault(); setEtape(etape + 1); }} >Suivant</button>
+            <button className="text-[16px] py-4 w-[9rem] rounded-[2rem] text-white bg-custom-black mr-4 cursor-pointer " onClick={(e) => { e.preventDefault(); setEtape(etape - 1); }}>Précédent</button>
+            <button className={`text-[16px] py-4 w-[9rem] rounded-[2rem] text-white cursor-pointer ${suivantSelected ? 'bg-custom-orange' : 'bg-[#9FA6B2]'}`} onClick={(e) => { e.preventDefault(); setEtape(etape + 1); }} disabled={!suivantSelected} >Suivant</button>
         </div>
     )
 }
