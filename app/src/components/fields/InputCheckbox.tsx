24e2bf26eab6b3325d8747808280de572c523856
SarahOtmane <sarahotmane02@gmail.com>
2025-04-26 16:19:18 +0200

    Rendre la propriété 'error' optionnelle dans le composant InputCheckbox et gérer la vérification des termes acceptés de manière sécurisée.

--- app/src/components/fields/InputCheckbox.tsx
+++ app/src/components/fields/InputCheckbox.tsx
@@ -3,14 +3,14 @@ import { Information } from "../../controllers/Register.controller";
 interface InputCheckboxProps {
     checked: boolean;
     onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
-    error: Information;
+    error?: Information;
 }
 
 const InputCheckbox: React.FC<InputCheckboxProps> = ({ checked, onChange, error }) => {
     return (
         <input 
             type="checkbox" 
-            className={`w-5 h-5 rounded-[1rem] border cursor-pointer accent-lilas focus:outline-none ${error.termsAccepted ? 'border-custom-red bg-red-background' : 'border-gray-dark'}`}
+            className={`w-5 h-5 rounded-[1rem] border cursor-pointer accent-lilas focus:outline-none ${error?.termsAccepted ? 'border-custom-red bg-red-background' : 'border-gray-dark'}`}
             checked={checked}
             onChange={onChange}
         />
