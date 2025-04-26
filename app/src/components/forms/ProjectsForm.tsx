19c9d5016ef379a9c9649f4b204a765ed38eba9f
SarahOtmane <sarahotmane02@gmail.com>
2025-04-27 01:07:38 +0200

    Ajout du composant ProjectsForm pour permettre le partage de projets significatifs.

---
+++ app/src/components/forms/ProjectsForm.tsx
@@ -0,0 +1,23 @@
+// import { useState } from "react";
+
+
+
+const ProjectsForm = () => {
+
+    // const [projectsAdded, setProjectsAdded] = useState(false);
+
+    return(
+        <div className="bg-white rounded-[1rem] py-12 px-10 mt-4 flex flex-col">
+            <h2 className="text-[24px] font-Jakarta-bold">Partagez vos projets les plus significatifs.</h2>
+
+            {/* { !projectsAdded && (
+                <form>
+
+                </form>
+            )} */}
+        
+        </div>
+    )
+}
+
+export default ProjectsForm;
