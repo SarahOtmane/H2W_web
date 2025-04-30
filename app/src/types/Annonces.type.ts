2db1b594aa92435d9d7f7e268691be5d5b5e0b87
SarahOtmane <sarahotmane02@gmail.com>
2025-04-30 10:36:59 +0200

    Ajout des interfaces AnnoncesType et CreateAnnoncesType pour la gestion des annonces.

---
+++ app/src/types/Annonces.type.ts
@@ -0,0 +1,41 @@
+export interface AnnoncesType {
+    id: number;
+    title: string;
+    location: string;
+    teleworking: string;
+    duration: string;
+    missions: string[];
+    profile: string[];
+    softSkills: string[];
+    hardSkills: string[];
+    software: string[];
+    companyDescription: string;
+    companyName: string;
+    remuneration : string;
+    avantages: string[];
+    derniereModif: string;
+    nbProfilMatch : number;
+    candidats : number;
+    statut : string
+}
+
+export interface CreateAnnoncesType {
+    title: string;
+    location: string;
+    teleworking: string;
+    duration: string;
+    missions: string[];
+    profile: string[];
+    softSkills: string[];
+    hardSkills: string[];
+    software: string[];
+    companyDescription: string;
+    companyName: string;
+    remuneration : string;
+    avantages: string[];
+    derniereModif: string;
+    nbProfilMatch : number;
+    candidats : number;
+    statut : string
+}
+
