1f3ef867e793ec6f6e9ea4b0a92d1e0641f78e70
SarahOtmane <sarahotmane02@gmail.com>
2025-04-30 10:37:27 +0200

    Ajout du composant InputRadio pour la gestion des options de sélection avec état contrôlé.

---
+++ app/src/components/fields/InputRadio.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+
+interface InputRadioProps {
+    checked: string;
+    setChecked: (value: string) => void;
+    option: string;
+}
+
+const InputRadio : React.FC<InputRadioProps> = ({option, checked, setChecked}) => {
+    return(
+        <label key={option} className="flex items-center gap-2 cursor-pointer">
+            <input
+                type="radio"
+                name="teletravail"
+                value={option}
+                checked={checked === option}
+                onChange={() => setChecked(option)}
+                className="accent-purple-600 w-4 h-4"
+            />
+            <span>{option}</span>
+        </label>
+    )
+}
+
+export default InputRadio;
