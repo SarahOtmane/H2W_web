1fb086a328427ea6cf5192499f701f16f5683b68
SarahOtmane <sarahotmane02@gmail.com>
2025-04-30 10:36:37 +0200

    Ajout de la gestion de l'état de connexion dans le composant Header pour afficher les boutons appropriés.

--- app/src/components/Header.tsx
+++ app/src/components/Header.tsx
@@ -1,5 +1,5 @@
 import { Link, useNavigate } from 'react-router-dom';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import Icon from '../utils/Icon';
 import ButtonBlack from './buttons/ButtonBlack';
@@ -12,12 +12,22 @@ interface HeaderProps {
 
 const Header : React.FC<HeaderProps> = ({style}) => {
     const navigate = useNavigate();
+    const [isConnected, setIsConnected] = useState<boolean>(false);
 
     const size = WindowSize();
     const isMobile = size.width < 768;
 
     const [isMenuOpen, setIsMenuOpen] = useState(false);
     const toggleMenu = () => setIsMenuOpen(prev => !prev);
+
+    useEffect(() => {
+        const isConnected = localStorage.getItem('isConnected');
+        if (isConnected) {
+            setIsConnected(true);
+        } else {
+            setIsConnected(false);
+        }
+    }, []);
 
     return (
         <header className={`flex flex-row justify-between items-center bg-white ${style==='white' ? 'md:bg-white' : 'md:bg-gray-background'} px-4 py-4 md:px-16 md:py-3 lg:px-38 lg:py-8`}>
@@ -37,7 +47,8 @@ const Header : React.FC<HeaderProps> = ({style}) => {
                     <MenuBurger isOpen={isMenuOpen} onClose={toggleMenu} />
                 </>
             )}
-            {!isMobile && <ButtonBlack text='Se connecter' icon={<Icon name='avatar' />} handleClick={()=>navigate('/login')}/>}
+            {!isMobile && !isConnected && <ButtonBlack text='Se connecter' icon={<Icon name='avatar' />} handleClick={()=>navigate('/login')}/>}
+            {!isMobile && isConnected && <ButtonBlack text='Mon compte' icon={<Icon name='avatar' />} handleClick={()=>navigate('/student/dashboard')}/>}
         </header>
     );
 }
