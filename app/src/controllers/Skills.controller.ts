aa928ac2e6ee9e7d9f84e80888177347e20e5375
SarahOtmane <sarahotmane02@gmail.com>
2025-04-30 22:47:57 +0200

    Ajout du contrôleur Skills pour gérer les compétences techniques et non techniques

---
+++ app/src/controllers/Skills.controller.ts
@@ -0,0 +1,53 @@
+
+const SkillsController = () =>{
+    const hardSkills = [
+        'Design graphique',
+        'HTML',
+        'CSS',
+        'UI/UX',
+        'React',
+        'Node.js',
+        'JavaScript',
+        'TypeScript',
+        'PHP',
+        'Python',
+        'Java',
+        'C++',
+    ];
+    const softwares = [
+        'Photoshop',
+        'Figma',
+        'Illustrator',
+        'WordPress',
+        'Adobe XD',
+        'Sketch',
+        'InVision',
+        'After Effects',
+        'Premiere Pro',
+        'Postman',
+        'Visual Studio Code',
+        'Sublime Text',
+        'Docker'
+    ];
+    const softSkills = [
+        'Communication claire',
+        'Travail d\'équipe',
+        'Résolution de problèmes',
+        'Gestion du temps',
+        'Créativité',
+        'Adaptabilité',
+        'Esprit critique',
+        'Gestion du stress',
+        'Leadership',
+        'Empathie',
+        'Prise de décision',
+    ];
+
+    return({
+        hardSkills,
+        softSkills,
+        softwares,
+    })
+}
+
+export default SkillsController;
