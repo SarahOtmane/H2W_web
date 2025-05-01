474a94d3431c5ba6ad06863c86a1118469308723
SarahOtmane <sarahotmane02@gmail.com>
2025-05-01 10:41:48 +0200

    Ajout de la gestion de l'état de l'étape dans le formulaire d'expérience et mise à jour des boutons de navigation.

--- app/src/components/forms/ExperienceForm.tsx
+++ app/src/components/forms/ExperienceForm.tsx
@@ -1,6 +1,5 @@
 import { useState } from "react";
 
-import CreatePortfolioButton from "../buttons/CreatePortfolioButton";
 import CreatePortfolioController from "../../controllers/CreatePortfolio.controller";
 import ButtonBlack from "../buttons/ButtonBlack";
 import InputCheckbox from "../fields/InputCheckbox"
@@ -10,11 +9,15 @@ import MissionsInput from "../student/MissionsInput"
 import Icon from "../../utils/Icon";
 import { Experience } from "../../types/Portfolio.types";
 
+interface ExperienceFormProps {
+    setEtape : (value: number) => void
+}
 
-const ExperienceForm = () => {
+
+const ExperienceForm : React.FC<ExperienceFormProps> = ({setEtape}) => {
 
     const {
-        etape, setEtape, portfolio, experience, setExperience, handleChangeExperienceCheckbox, handleAddExperienceInPortfolio, handleDeleteExperience
+        portfolio, experience, setExperience, handleChangeExperienceCheckbox, handleAddExperienceInPortfolio, handleDeleteExperience
     } = CreatePortfolioController()
 
     let experiences = portfolio.experiences;
@@ -61,19 +64,19 @@ const ExperienceForm = () => {
                                     >
                                         <svg width="51" height="51" viewBox="0 0 51 51" fill="none" xmlns="http://www.w3.org/2000/svg">
                                             <rect width="50.6757" height="50.6757" rx="25.3378" fill="#8C73FF"/>
-                                            <path d="M35.4734 18.5582C31.7234 18.1866 27.9509 17.9951 24.1896 17.9951C21.9599 17.9951 19.7302 18.1077 17.5004 18.333L15.2031 18.5582" stroke="white" stroke-width="2.53378" stroke-linecap="round" stroke-linejoin="round"/>
-                                            <path d="M21.3945 17.4208L21.6423 15.9455C21.8225 14.8757 21.9576 14.0762 23.8607 14.0762H26.8112C28.7144 14.0762 28.8607 14.9208 29.0297 15.9568L29.2774 17.4208" stroke="white" stroke-width="2.53378" stroke-linecap="round" stroke-linejoin="round"/>
-                                            <path d="M33.0529 22.1182L32.3209 33.4583C32.1971 35.2263 32.0957 36.6001 28.9538 36.6001H21.7241C18.5822 36.6001 18.4809 35.2263 18.357 33.4583L17.625 22.1182" stroke="white" stroke-width="2.53378" stroke-linecap="round" stroke-linejoin="round"/>
-                                            <path d="M23.457 30.4053H27.207" stroke="white" stroke-width="2.53378" stroke-linecap="round" stroke-linejoin="round"/>
-                                            <path d="M22.5234 25.9004H28.1541" stroke="white" stroke-width="2.53378" stroke-linecap="round" stroke-linejoin="round"/>
+                                            <path d="M35.4734 18.5582C31.7234 18.1866 27.9509 17.9951 24.1896 17.9951C21.9599 17.9951 19.7302 18.1077 17.5004 18.333L15.2031 18.5582" stroke="white" strokeWidth="2.53378" strokeLinecap="round" strokeLinejoin="round"/>
+                                            <path d="M21.3945 17.4208L21.6423 15.9455C21.8225 14.8757 21.9576 14.0762 23.8607 14.0762H26.8112C28.7144 14.0762 28.8607 14.9208 29.0297 15.9568L29.2774 17.4208" stroke="white" strokeWidth="2.53378" strokeLinecap="round" strokeLinejoin="round"/>
+                                            <path d="M33.0529 22.1182L32.3209 33.4583C32.1971 35.2263 32.0957 36.6001 28.9538 36.6001H21.7241C18.5822 36.6001 18.4809 35.2263 18.357 33.4583L17.625 22.1182" stroke="white" strokeWidth="2.53378" strokeLinecap="round" strokeLinejoin="round"/>
+                                            <path d="M23.457 30.4053H27.207" stroke="white" strokeWidth="2.53378" strokeLinecap="round" strokeLinejoin="round"/>
+                                            <path d="M22.5234 25.9004H28.1541" stroke="white" strokeWidth="2.53378" strokeLinecap="round" strokeLinejoin="round"/>
                                         </svg>
                                     </button>
                                     <button className=" cursor-pointer">
                                         <svg width="51" height="51" viewBox="0 0 51 51" fill="none" xmlns="http://www.w3.org/2000/svg">
                                             <rect x="0.324219" width="50.6757" height="50.6757" rx="25.3378" fill="#FF9B3E"/>
-                                            <path d="M24.5375 14.0762H22.2852C16.6546 14.0762 14.4023 16.3284 14.4023 21.9591V28.7158C14.4023 34.3464 16.6546 36.5987 22.2852 36.5987H29.042C34.6726 36.5987 36.9249 34.3464 36.9249 28.7158V26.4636" stroke="white" stroke-width="2.53378" stroke-linecap="round" stroke-linejoin="round"/>
-                                            <path d="M30.211 15.2255L21.3371 24.0994C20.9993 24.4372 20.6614 25.1016 20.5939 25.5859L20.1096 28.9755C19.9295 30.203 20.7966 31.0588 22.0241 30.8899L25.4137 30.4057C25.8867 30.3381 26.5511 30.0003 26.9002 29.6624L35.7741 20.7886C37.3056 19.257 38.0263 17.4778 35.7741 15.2255C33.5218 12.9733 31.7425 13.694 30.211 15.2255Z" stroke="white" stroke-width="2.53378" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
-                                            <path d="M28.9375 16.498C29.692 19.1895 31.7979 21.2953 34.5006 22.0611" stroke="white" stroke-width="2.53378" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
+                                            <path d="M24.5375 14.0762H22.2852C16.6546 14.0762 14.4023 16.3284 14.4023 21.9591V28.7158C14.4023 34.3464 16.6546 36.5987 22.2852 36.5987H29.042C34.6726 36.5987 36.9249 34.3464 36.9249 28.7158V26.4636" stroke="white" strokeWidth="2.53378" strokeLinecap="round" strokeLinejoin="round"/>
+                                            <path d="M30.211 15.2255L21.3371 24.0994C20.9993 24.4372 20.6614 25.1016 20.5939 25.5859L20.1096 28.9755C19.9295 30.203 20.7966 31.0588 22.0241 30.8899L25.4137 30.4057C25.8867 30.3381 26.5511 30.0003 26.9002 29.6624L35.7741 20.7886C37.3056 19.257 38.0263 17.4778 35.7741 15.2255C33.5218 12.9733 31.7425 13.694 30.211 15.2255Z" stroke="white" strokeWidth="2.53378" strokeMiterlimit="10" strokeLinecap="round" strokeLinejoin="round"/>
+                                            <path d="M28.9375 16.498C29.692 19.1895 31.7979 21.2953 34.5006 22.0611" stroke="white" strokeWidth="2.53378" strokeMiterlimit="10" strokeLinecap="round" strokeLinejoin="round"/>
                                         </svg>
                                     </button>
                                 </div>
@@ -104,7 +107,7 @@ const ExperienceForm = () => {
                                                 >
                                                     <span className="mr-2">Voir plus</span>
                                                     <svg width="19" height="19" viewBox="0 0 19 19" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                                        <path d="M15.7705 7.08496L10.6088 12.2466C9.99922 12.8562 9.00172 12.8562 8.39214 12.2466L3.23047 7.08496" stroke="#1D1D1B" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
+                                                        <path d="M15.7705 7.08496L10.6088 12.2466C9.99922 12.8562 9.00172 12.8562 8.39214 12.2466L3.23047 7.08496" stroke="#1D1D1B" strokeWidth="1.5" strokeMiterlimit="10" strokeLinecap="round" strokeLinejoin="round"/>
                                                     </svg>
                                                 </button>
                                             }
@@ -118,7 +121,7 @@ const ExperienceForm = () => {
                                                 >
                                                     <span className="mr-2">Voir moins</span>
                                                     <svg width="19" height="19" viewBox="0 0 19 19" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                                        <path d="M3.23047 11.915L8.39214 6.75342C9.00172 6.14384 9.99922 6.14384 10.6088 6.75342L15.7705 11.915" stroke="#1D1D1B" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
+                                                        <path d="M3.23047 11.915L8.39214 6.75342C9.00172 6.14384 9.99922 6.14384 10.6088 6.75342L15.7705 11.915" stroke="#1D1D1B" strokeWidth="1.5" strokeMiterlimit="10" strokeLinecap="round" strokeLinejoin="round"/>
                                                     </svg>
                                                 </button>
                                             }
@@ -241,10 +244,10 @@ const ExperienceForm = () => {
                     </button>
                 </form>
             )}
-            <CreatePortfolioButton 
-                suivantSelected={(experiences.length > 0) ? true : false} 
-                setEtape={setEtape} etape={etape} 
-            />
+            <div className="flex flex-row justify-center items-center mt-15">
+                <button className="text-[16px] py-4 w-[9rem] rounded-[2rem] text-white bg-custom-black mr-4 cursor-pointer " onClick={(e) => { e.preventDefault(); setEtape(2); }}>Précédent</button>
+                <button className={`text-[16px] py-4 w-[9rem] rounded-[2rem] text-white cursor-pointer ${experiences.length>0 ? 'bg-custom-orange' : 'bg-[#9FA6B2]'}`} onClick={(e) => { e.preventDefault(); setEtape(4); }} disabled={!(experiences.length>0)} >Suivant</button>
+            </div>
         </div>
     )
 }
